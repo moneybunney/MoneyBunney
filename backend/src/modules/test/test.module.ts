a00801ecbe7eda788818bfacd2edd72cace45fd8
Andrius <and.bankauskas@gmail.com>
2019-02-20 22:45:18 +0200

    fix bad gitignore

--- backend/src/modules/test/test.module.ts
+++ backend/src/modules/test/test.module.ts
@@ -4,6 +4,7 @@ import { TestController } from './test.controller';
 import { TestService } from './test.service';
 import { TestSchema } from './schemas/test.schema';
 
+// test module for playing around and testing db configuration
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Test', schema: TestSchema }])],
   controllers: [TestController],

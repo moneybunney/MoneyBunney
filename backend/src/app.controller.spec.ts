bca17810dc4e3f6e0decd7864ea861b74e600692
Andrius-B <and.bankauskas@gmail.com>
2019-02-28 18:19:41 +0200

    Added example controller test

---
+++ backend/src/app.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return \"Index of MonneyBunney\"', () => {
+      expect(appController.getApp()).toBe('Index of MonneyBunney');
+    });
+  });
+});

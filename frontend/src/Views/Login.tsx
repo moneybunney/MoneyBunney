d364919b87b25ac9022961794687d0a8f45dc489
Martynas Stankevičius <martynoemailas@gmail.com>
2019-03-03 22:21:21 +0200

    Fix tslint problems and autoformat with prettier

--- frontend/src/Views/Login.tsx
+++ frontend/src/Views/Login.tsx
@@ -1,47 +1,60 @@
 import {
-  Avatar, Button, Checkbox, CssBaseline, FormControl, FormControlLabel,
-  Input, InputLabel, Paper, Typography, CircularProgress, FormHelperText
+  Avatar,
+  Button,
+  Checkbox,
+  CircularProgress,
+  createStyles,
+  CssBaseline,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Paper,
+  Theme,
+  Typography,
+  WithStyles,
+  withStyles,
 } from "@material-ui/core";
-import { createStyles } from "@material-ui/core/styles";
-import { Theme } from "@material-ui/core/styles/createMuiTheme";
-import withStyles, { WithStyles } from "@material-ui/core/styles/withStyles";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { LockOutlined } from "@material-ui/icons";
 import React from "react";
 
-const styles = (theme: Theme) => createStyles({
-  main: {
-    width: "auto",
-    display: "block", // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto",
+const styles = (theme: Theme) =>
+  createStyles({
+    main: {
+      width: "auto",
+      display: "block", // Fix IE 11 issue.
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+        width: 400,
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
     },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-  progress: {
-    margin: theme.spacing.unit * 2,
-  },
-});
+    paper: {
+      marginTop: theme.spacing.unit * 8,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+        .spacing.unit * 3}px`,
+    },
+    avatar: {
+      margin: theme.spacing.unit,
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing.unit,
+    },
+    submit: {
+      marginTop: theme.spacing.unit * 3,
+    },
+    progress: {
+      margin: theme.spacing.unit * 2,
+    },
+  });
 
 interface IProps extends WithStyles<typeof styles> {}
 
@@ -68,8 +81,8 @@ function SignIn(props: IProps) {
     setLoading(true);
 
     setTimeout(() => {
-      if(username === 'username' && password === 'password'){
-        alert('Login success');
+      if (username === "username" && password === "password") {
+        alert("Login success");
       } else {
         setError(true);
       }
@@ -82,27 +95,52 @@ function SignIn(props: IProps) {
       <CssBaseline />
       <Paper className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <LockOutlined/>
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
-          {loading &&
-            <CircularProgress className={classes.progress} />
-          }
+          {loading && <CircularProgress className={classes.progress} />}
         </Typography>
 
-
         <form className={classes.form} onSubmit={handleSubmit}>
-          <FormControl margin="normal" required={true} fullWidth={true} disabled={loading} error={error}>
+          <FormControl
+            margin="normal"
+            required={true}
+            fullWidth={true}
+            disabled={loading}
+            error={error}
+          >
             <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus={true} onChange={onUsernameChange} value={username} />
+            <Input
+              id="email"
+              name="email"
+              autoComplete="email"
+              autoFocus={true}
+              onChange={onUsernameChange}
+              value={username}
+            />
           </FormControl>
-          <FormControl margin="normal" required={true} fullWidth={true} disabled={loading} error={error}>
+          <FormControl
+            margin="normal"
+            required={true}
+            fullWidth={true}
+            disabled={loading}
+            error={error}
+          >
             <InputLabel htmlFor="password">Password</InputLabel>
-            <Input name="password" type="password" id="password" autoComplete="current-password" onChange={onPasswordChange} value={password} />
-            {error &&
-              <FormHelperText id="component-error-text">Wrong password</FormHelperText>
-            }
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={onPasswordChange}
+              value={password}
+            />
+            {error && (
+              <FormHelperText id="component-error-text">
+                Wrong password
+              </FormHelperText>
+            )}
           </FormControl>
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}

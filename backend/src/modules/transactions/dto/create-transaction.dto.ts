98c69411d671e5dd5a99e97cdf232012742a5905
Klaidas <klaidaz01@gmail.com>
2019-03-08 18:25:07 +0200

    add validation pipe

--- backend/src/modules/transactions/dto/create-transaction.dto.ts
+++ backend/src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,8 +1,20 @@
+import { IsString, IsNumber, IsDate, IsArray} from 'class-validator';
+
 export class CreateTransactionDto {
     readonly Date: object;
+
+    @IsString()
     readonly Account: string;
+
+    @IsString()
     readonly Category: string;
+
+    @IsNumber()
     readonly Price: number;
+
+    @IsString()
     readonly Description: string;
+
+    @IsArray()
     readonly Tags: string[];
 }
--- backend/src/modules/transactions/transactions.controller.ts
+++ backend/src/modules/transactions/transactions.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UsePipes } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Transactions } from './interfaces/transactions.interface';
+import { ValidationPipe } from '../../common/pipes/validation.pipe'
 
 @Controller('transactions')
 export class TransactionsController {
@@ -9,6 +10,7 @@ export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   async create(@Body() createTransactionDto: CreateTransactionDto) {
     console.log('Transaction received:');
     console.log(CreateTransactionDto);
@@ -17,13 +19,15 @@ export class TransactionsController {
 
   @Get()
   getAllTests(): Promise<Transactions[]> {
+    var temp = new Date();
+    console.log(temp);
     console.log(`GET to /transactions | getAllTransactions`);
     return this.transactionsService.findAll();
   }
 
   @Get(':id')
   getTest(@Param('id') id: string): Promise<Transactions> {
-    console.log('GET to /test | getTest');
+    console.log('GET to /transactions | getTransaction');
     return this.transactionsService.findById(id);
   }
 }

92bc0c209722cb6aa9a9637fb7d124af9e92f3a8
Andrius <and.bankauskas@gmail.com>
2019-03-08 20:47:42 +0200

    Added initial item list prototype

--- frontend/src/Views/TransactionList/TransactionList.tsx
+++ frontend/src/Views/TransactionList/TransactionList.tsx
@@ -1,16 +1,31 @@
-import { createStyles, Theme, WithStyles, withStyles } from "@material-ui/core";
+import { createStyles, List, Theme, WithStyles, withStyles} from "@material-ui/core";
+import {mdiBeer} from "@mdi/js";
+import { Icon } from "@mdi/react";
 import React from "react";
+import { createEmptyTransaction } from "../../Models/TransactionModel";
+import TransactionListItem from "./TransactionListItem";
 
 const styles = (theme: Theme) => createStyles({
-
+    listRoot: {
+        width: "100%",
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
 });
 
 interface IProps extends WithStyles<typeof styles> {
 }
 
-const TransactionList = (props: IProps) => {
+const TransactionList = ({classes}: IProps) => {
+    const transactions = [1, 2, 3].map((i) => {
+        const transaction = createEmptyTransaction();
+        transaction.category = i;
+        return transaction;
+    });
     return (
-        <div>Hello transaction list!</div>
+        <List>
+            {transactions.map((t, i) => <TransactionListItem key={"transaction_" + i} transaction={t}/>)}
+        </List>
     );
 };
 
---
+++ frontend/src/Views/TransactionList/TransactionListItemIcon.tsx
@@ -0,0 +1,51 @@
+import { Avatar, createStyles, Theme, WithStyles, withStyles } from "@material-ui/core";
+import { mdiBankTransfer, mdiBeer, mdiGlassWine } from "@mdi/js";
+import Icon from "@mdi/react";
+import React from "react";
+
+const iconMap = {
+    1 : mdiBeer,
+    2 : mdiGlassWine,
+    3 : mdiBankTransfer,
+};
+
+interface IProps {
+    iconId: number;
+
+    // measurements in pixels
+    size?: number;
+    margin?: number;
+    innerSize?: number;
+    color?: string | null;
+}
+
+const TransactionListItemIcon = ({ iconId, margin, innerSize, size, color }: IProps) => {
+
+    const resolveIconPath = (id: number) => {
+        return (iconMap as any)[id];
+    };
+    // setting the defaults:
+    // height / width => 48px
+    // inner icon size => 3/4 * size
+    // color => #fafafa
+
+    const totalSizeFinal = size ? size : 48;
+    const innerSizeFinal = innerSize ? innerSize : totalSizeFinal * 3 / 4;
+    const colorFinal = color ? color : "#fafafa";
+    const avatarStyles = {
+        width: totalSizeFinal,
+        height: totalSizeFinal,
+        margin,
+    };
+    return (
+        <Avatar style={avatarStyles}>
+            <Icon
+                path={resolveIconPath(iconId)}
+                size={(innerSizeFinal) + "px"}
+                color={colorFinal}
+            />
+        </Avatar>
+    );
+};
+
+export default TransactionListItemIcon;

7be2a67bac8be84d38736f569fae499765e0600e
Martynas Stankevičius <martynoemailas@gmail.com>
2019-03-12 21:31:49 +0200

    Made all the routes in the <App /> redirect based on auth status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,14 +10,41 @@ import Checkout from "./Views/Checkout/Checkout";
 import Login from "./Views/Login/Login";
 import Register from "./Views/Register/Register";
 
+import RouteWithAuthentication from "./Components/RouteWithAuthentication";
+
+const DummyMainPage = () => {
+  return <div>Delete the cookies to see the login page</div>;
+};
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact={true} path="/" component={Login} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/checkout" component={Checkout} />
+        <RouteWithAuthentication
+          onlyLoggedIn={true}
+          redirectRoute="/login"
+          exact={true}
+          path="/"
+          Component={DummyMainPage}
+        />
+        <RouteWithAuthentication
+          onlyLoggedIn={false}
+          redirectRoute="/"
+          path="/login"
+          Component={Login}
+        />
+        <RouteWithAuthentication
+          onlyLoggedIn={false}
+          redirectRoute="/"
+          path="/register"
+          Component={Register}
+        />
+        <RouteWithAuthentication
+          onlyLoggedIn={true}
+          redirectRoute="/login"
+          path="/checkout"
+          Component={Checkout}
+        />
         <Redirect to="/" />
       </Switch>
     </Router>

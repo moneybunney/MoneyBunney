92cf5701cc50796dd5e62496cee013fdef928b58
Martynas Stankevičius <martynoemailas@gmail.com>
2019-03-12 21:42:12 +0200

    Add the correct types to <RouteWithAuthentication />

--- frontend/src/Components/RouteWithAuthentication.tsx
+++ frontend/src/Components/RouteWithAuthentication.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Redirect, Route, RouteComponentProps } from "react-router-dom";
+import { Redirect, Route, RouteComponentProps, RouteProps } from "react-router-dom";
 import { isAuthenticated } from "../Utilities/AuthenticationCookies";
 
 interface IProps {
   Component: React.ComponentType;
   onlyLoggedIn: boolean;
   redirectRoute: string;
-  path: string;
-  exact?: boolean;
 }
 
 const RouteWithAuthentication = ({
   Component,
   onlyLoggedIn,
   redirectRoute,
-  path,
-  exact,
-}: IProps) => {
+  ...rest
+}: IProps & RouteProps) => {
 
-  const render = (props: any) =>
+  const render = (props: RouteComponentProps<any>) =>
     isAuthenticated() === onlyLoggedIn ? (
       <Component />
     ) : (
@@ -30,42 +27,7 @@ const RouteWithAuthentication = ({
       />
     );
 
-  return <Route path={path} exact={exact ? exact : false} render={render} />;
+  return <Route {...rest} render={render} />;
 };
-
-// This is how it should work, with the ...rest props
-// But it doesn't because of typescript
-// TODO: Fix
-
-// interface IProps {
-//   Component: React.ComponentType;
-//   onlyLoggedIn: boolean;
-//   redirectRoute: string;
-// }
-
-// const RouteWithAuthentication = ({
-//   Component,
-//   onlyLoggedIn,
-//   redirectRoute,
-//   ...rest
-// }: RouteComponentProps<any> & IProps) => {
-//   return (
-//     <Route
-//       {...rest}
-//       render={props =>
-//         isAuthenticated() === onlyLoggedIn ? (
-//           <Component />
-//         ) : (
-//           <Redirect
-//             to={{
-//               pathname: redirectRoute,
-//               state: { from: props.location },
-//             }}
-//           />
-//         )
-//       }
-//     />
-//   );
-// };
 
 export default RouteWithAuthentication;

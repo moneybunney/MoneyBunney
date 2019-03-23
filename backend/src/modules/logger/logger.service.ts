0fe2e4ab8fae1137fc5d02125a4df994fede9880
Klaidas <klaidaz01@gmail.com>
2019-03-23 11:53:56 +0200

    add logger from development branch

---
+++ backend/src/modules/logger/logger.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, LoggerService } from '@nestjs/common';
+
+@Injectable()
+export class Logger implements LoggerService {
+    log(message: string) {
+        console.log(message);
+    }
+    error(message: string, trace: string){
+        console.error(message);
+    }
+    warn(message: string){
+        console.warn(message);
+    }
+}

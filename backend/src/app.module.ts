b7d64882a77f7dc00eadd3436aced77a2c515861
Martynas Stankevicius <martynoemailas@gmail.com>
2019-03-26 14:50:20 +0200

    Make the ConfigService validate env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,21 +9,13 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from './modules/config/config.module';
 import { ConfigService } from './modules/config/config.service';
 
-const constructUri = async (configService: ConfigService) => {
-  const DB_HOST = configService.get('DB_HOST');
-  const DB_PORT = configService.get('DB_PORT');
-  const DB_NAME = configService.get('DB_NAME');
-
-  const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-
-  return { uri };
-};
-
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: constructUri,
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.databaseUri,
+      }),
       inject: [ConfigService],
     }),
     TestModule,
--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -1,19 +1,57 @@
 import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
 import * as fs from 'fs';
 
+interface EnvConfig {
+  [key: string]: string;
+}
+
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private readonly envConfig: EnvConfig;
 
   constructor(filePath: string) {
+    let config;
+
     if (fs.existsSync(filePath)) {
       const dotEnvData = dotenv.parse(fs.readFileSync(filePath));
-      this.envConfig = Object.assign({}, dotEnvData, process.env);
+      config = { ...dotEnvData, ...process.env };
     } else {
-      this.envConfig = process.env;
+      config = process.env;
     }
+    this.envConfig = this.validateInput(config);
   }
 
-  get(key: string): string {
-    return this.envConfig[key];
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test'])
+        .default('development'),
+      DB_PORT: Joi.number().default(27017),
+      DB_HOST: Joi.string().default('localhost'),
+      DB_NAME: Joi.string().default('moneybunney'),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+      {
+        allowUnknown: true,
+      },
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
+  }
+
+  get databaseUri(): string {
+    const { DB_HOST, DB_PORT, DB_NAME } = this.envConfig;
+    const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+    return uri;
   }
 }

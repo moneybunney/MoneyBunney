402c6a7f647997fb2889344cfc2fbe743db3c4f9
Andrius <and.bankauskas@gmail.com>
2019-03-27 20:34:32 +0200

    Cleaned up query DTO's

--- backend/src/modules/transactions/dto/selector.dto.ts
+++ backend/src/modules/transactions/dto/selector.dto.ts
@@ -1,10 +1,7 @@
 import { IsString } from 'class-validator';
 
 export class SelectorDTO {
-    @IsString({message: 'Name must be a string!'})
     readonly Name: string;
-
     readonly Key?: any;
-
     readonly Value?: any;
 }

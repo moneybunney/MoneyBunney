4e93eec0a4ce83bcacaefd953899ee61f44eb6c4
Roberta Milasauskaite <roberta.milasauskaite@gmail.com>
2019-04-24 06:02:19 +0300

    Remove _v from schema

--- backend/src/modules/transactions/schemas/transactions.schema.ts
+++ backend/src/modules/transactions/schemas/transactions.schema.ts
@@ -1,10 +1,13 @@
 import * as mongoose from 'mongoose';
 
-export const TransactionsSchema = new mongoose.Schema({
-  Date,
-  Account: String,
-  Category: String,
-  Amount: Number,
-  Description: String,
-  Tags: [String],
-});
+export const TransactionsSchema = new mongoose.Schema(
+  {
+    Date,
+    Account: String,
+    Category: String,
+    Amount: Number,
+    Description: String,
+    Tags: [String],
+  },
+  { versionKey: false },
+);

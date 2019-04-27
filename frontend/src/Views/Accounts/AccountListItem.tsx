c25abf8cf6a90a67e264fe6d2a6b71e7c1c9f89a
Roberta Milasauskaite <roberta.milasauskaite@gmail.com>
2019-04-28 00:28:25 +0300

    Fix uneccessary change

--- frontend/src/Views/Accounts/AccountListItem.tsx
+++ frontend/src/Views/Accounts/AccountListItem.tsx
@@ -9,7 +9,7 @@ import React from "react";
 import useReactRouter from "use-react-router";
 import BalanceAmountText from "../../Components/BalanceAmountText";
 import { IAccount } from "../../Models/AccountModel";
-import { AccountsLocation } from "../../routes.constants";
+import { TransactionsLocation } from "../../routes.constants";
 
 const useStyles = makeStyles((theme: Theme) => ({}));
 
@@ -23,7 +23,7 @@ const AccountListItem = ({ account }: IProps) => {
 
   const onClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
     event.preventDefault();
-    history.replace(`${AccountsLocation}?account=${account.name}`);
+    history.replace(`${TransactionsLocation}?account=${account.name}`);
   };
 
   return (

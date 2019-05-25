62fc2b9aa8e70ab42d6b5f6a30b8470b26123ce7
Martynas Stankevičius <martynoemailas@gmail.com>
2019-05-25 16:01:53 +0300

    Fix up <BudgetListItem /> component
    - Extract only the needed part of the data in the toDisplayDate()
      function
    - Remove unused state `shown`
    - Remove unecessary `<Collapse />`
    - Make `<ListItem />` not be a button for no reason

--- frontend/src/Components/BudgetList/BudgetListItem.tsx
+++ frontend/src/Components/BudgetList/BudgetListItem.tsx
@@ -2,8 +2,8 @@ import { Collapse, ListItem, ListItemText, Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React, { useEffect } from "react";
 import { IBudget } from "../../Models/BudgetModel";
-import ProgressBar from "../ProgressBar";
 import TransactionListItemIcon from "../../Views/TransactionList/TransactionListItemIcon";
+import ProgressBar from "../ProgressBar";
 
 const useStyles = makeStyles((theme: Theme) => ({}));
 
@@ -19,10 +19,7 @@ const toDisplayDate = (d: Date) => {
   const month = pad(d.getMonth() + 1);
   const day = pad(d.getDate());
   const date = d.getFullYear() + "-" + month + "-" + day;
-  const hours = pad(d.getHours());
-  const minutes = pad(d.getMinutes());
-  const time = hours + ":" + minutes;
-  return date + " " + time;
+  return date;
 };
 
 const BudgetListItem = ({
@@ -32,32 +29,25 @@ const BudgetListItem = ({
   load
 }: IProps) => {
   const classes = useStyles();
-  const [shown, setShown] = React.useState(false);
-
-  useEffect(() => {
-    setShown(true);
-  });
 
   const primaryText = categoryText;
 
   const parsedStartDate = new Date(budget.startDate);
   const parsedEndDate = new Date(budget.endDate);
 
-  const startDateString = toDisplayDate(parsedStartDate).split(" ")[0];
-  const endDateString = toDisplayDate(parsedEndDate).split(" ")[0];
+  const startDateString = toDisplayDate(parsedStartDate);
+  const endDateString = toDisplayDate(parsedEndDate);
   return (
-    <Collapse in={shown}>
-      <ListItem button={true}>
-        <TransactionListItemIcon
-          iconId={categoryIcon !== "" ? categoryIcon : undefined}
-        />
-        <ListItemText
-          primary={primaryText + " " + startDateString + " - " + endDateString}
-          secondary={load + "/" + budget.amount + "\u20AC"}
-        />
-        <ProgressBar capacity={budget.amount} load={load} />
-      </ListItem>
-    </Collapse>
+    <ListItem>
+      <TransactionListItemIcon
+        iconId={categoryIcon !== "" ? categoryIcon : undefined}
+      />
+      <ListItemText
+        primary={primaryText + " " + startDateString + " - " + endDateString}
+        secondary={load + "/" + budget.amount + "\u20AC"}
+      />
+      <ProgressBar capacity={budget.amount} load={load} />
+    </ListItem>
   );
 };
 

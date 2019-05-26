d1364ed65933e2696bd999506de1fc7fd418550a
Andrius <and.bankauskas@gmail.com>
2019-05-26 15:24:30 +0300

    Deletion dialog

--- frontend/src/Components/DeleteItemButton.tsx
+++ frontend/src/Components/DeleteItemButton.tsx
@@ -1,4 +1,13 @@
-import { Button, CircularProgress, Theme } from "@material-ui/core";
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Theme
+} from "@material-ui/core";
 import { Clear } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/styles";
 import React, { useState } from "react";
@@ -21,18 +30,57 @@ const DeleteItemButton = ({
 }) => {
   const classes = useStyles();
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = React.useState(false);
 
-  const deleteSubmit = async () => {
-    setLoading(true);
-    // await delet(path, params);
-    setLoading(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOk = async () => {
+    handleClose();
+    await delet(path, params);
     onDeleted();
   };
 
-  return loading ? (
-    <CircularProgress size={24} className={classes.common} />
-  ) : (
-    <Clear onClick={deleteSubmit} className={classes.common} />
+  const handleCancel = () => {
+    handleClose();
+    setLoading(false);
+  };
+
+  const deleteSubmit = async () => {
+    setLoading(true);
+    setOpen(true);
+  };
+
+  return (
+    <>
+      {loading ? (
+        <CircularProgress size={24} className={classes.common} />
+      ) : (
+        <Clear onClick={deleteSubmit} className={classes.common} />
+      )}
+      <Dialog
+        open={open}
+        onClose={handleCancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Are you sure?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This action can not be undone!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleOk} color="primary" autoFocus={true}>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
